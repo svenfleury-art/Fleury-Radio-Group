7dc6fa600415334bfe305d9ee55c78f86c21a28a
FRG <svenfleury@icloud.com>
2026-04-16 11:58:10 +0200

    Update main.js

--- main.js
+++ main.js
@@ -405,15 +405,15 @@ const stations = document.querySelectorAll(".station");
 const streams = {
   rhywaelle: {
     name: "Radio Rhywälle",
-    url: "https://dein-stream-link/rhywaelle.mp3"
+    url: "https://stream.laut.fm/rhywaelle"
   },
   winterlord: {
     name: "Winterlord FM",
-    url: "https://dein-stream-link/winterlord.mp3"
+    url: "https://stream.laut.fm/winterlord-fm"
   },
   rhyrock: {
     name: "RhyRock Radio",
-    url: "https://dein-stream-link/rhyrock.mp3"
+    url: "https://stream.laut.fm/rhyrock-radio"
   }
 };
 
