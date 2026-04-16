84af7f9e457d6ef7ddbe08909b3a1177c5ec481f
FRG <svenfleury@icloud.com>
2026-04-16 21:02:39 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,3 +1,17 @@
+async function loadPartial(id, file) {
+  const el = document.getElementById(id);
+  if (!el) return;
+
+  try {
+    const res = await fetch(file);
+    const html = await res.text();
+    el.innerHTML = html;
+  } catch (err) {
+    console.error("Partial Fehler:", file, err);
+  }
+}
+
+
 /* =========================
 ROUTES
 ========================= */
