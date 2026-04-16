d3289e94f0a2aa1958274582f36d3c30c866ff7a
FRG <svenfleury@icloud.com>
2026-04-16 17:18:26 +0200

    Update main.js

--- main.js
+++ main.js
@@ -163,11 +163,17 @@ COUNTDOWN (FIXED)
 ========================= */
 
 const frgEvents = [
-  { title: "FRG Showcase Week", date: "2026-03-23T00:00:00" },
-  { title: "Labirinth Premiere", date: "2026-03-24T12:30:00" },
-  { title: "Oldies Special", date: "2026-03-30T15:00:00" },
+   { title: "FRG Showcase Week", date: "2026-04-17T00:00:00" },
   { title: "FRG Crossover Night", date: "2026-04-25T20:00:00" },
-  { title: "FRG Showcase Week", date: "2026-04-26T15:00:00" }
+  { title: "FRG Simulcast", date: "2026-05-30T19:00:00" },
+  { title: "FRG Crossover Night", date: "2026-06-27T19:00:00" },
+  { title: "FRG Schweiz Special", date: "2026-08-01T12:00:00" },
+  { title: "FRG Crossover Night", date: "2026-09-26T19:00:00" },
+  { title: "1 Jahr Fleury Radio Group", date: "2026-10-28T12:00:00" },
+  { title: "FRG Halloween Special", date: "2026-10-31T12:00:00" },
+  { title: "FRG Crossover Night", date: "2026-11-28T20:00:00" },
+  { title: "FRG Weihnachts Special", date: "2026-12-19T00:00:00" },
+  { title: "FRG Neujahres Special", date: "2026-12-31T13:00:00" }
 ];
 
 const isHome = document.body.dataset.page === "home";
